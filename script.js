161ffcae9929e9673f2f7b0629ba456334ef14de
alejandro20050830 <96395408+alejandro20050830@users.noreply.github.com>
2024-09-12 23:37:10 -0400

    Update script.js

--- script.js
+++ script.js
@@ -22,12 +22,12 @@
         const enlaceAleatorio = enlacesAdqva[Math.floor(Math.random() * enlacesAdqva.length)];
         boton.href = enlaceAleatorio;
       } else {
-        console.log("No se encontraron enlaces válidos.");
+        alert("No se encontraron enlaces válidos.");
       }
     }
 
     // Ejecutar la función cada 20 segundos
-    setInterval(actualizarEnlaceBoton, 20000); 
+    setInterval(actualizarEnlaceBoton, 5000); 
 
     // Ejecutar la función al cargar la página
     actualizarEnlaceBoton(); // Llamada inicial para actualizar al cargar

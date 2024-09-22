1f39117bc377c9396d4f7edd14b39c21bff87b76
alejandro20050830 <96395408+alejandro20050830@users.noreply.github.com>
2024-09-22 17:55:13 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,47 @@
-  // Función para obtener los enlaces Adqva
-alert("yes");
+console.log("yes");
 function obtenerEnlacesAdqva() {
-      const elementosImagen = document.querySelectorAll("._AdQVA_ad_unit_image");
-      const enlaces = [];
+  const allElementsWithClassA = [];
+  const allElements = document.querySelectorAll("*"); // Selecciona todos los elementos de la página
 
-      elementosImagen.forEach(elemento => {
-        const nodoHijo = elemento.querySelector("a[href*='https://servicer.adqva.com/click/']");
-        if (nodoHijo) {
-          enlaces.push(nodoHijo.href);
-        }
-      });
-
-      return enlaces;
+  for (const element of allElements) {
+    if (element.shadowRoot) {
+      // Verifica si el elemento tiene un shadowRoot
+      const elementsInsideShadowRoot = element.shadowRoot.querySelectorAll(
+        "._AdQVA_ad_unit_image"
+      );
+      allElementsWithClassA.push(...elementsInsideShadowRoot); // Agrega los elementos encontrados a la lista
     }
+  }
 
-    // Obtener los enlaces y actualizar el botón
-    function actualizarEnlaceBoton() {
-      const enlacesAdqva = obtenerEnlacesAdqva();
-      const boton = document.getElementById("btn"); 
+  const enlaces = [];
 
-      if (enlacesAdqva.length > 0) {
-        const enlaceAleatorio = enlacesAdqva[Math.floor(Math.random() * enlacesAdqva.length)];
-        boton.href = enlaceAleatorio;
-      } else {
-        alert("No se encontraron enlaces válidos.");
-      }
+  allElementsWithClassA.forEach((elemento) => {
+    const nodoHijo = elemento.querySelector("a");
+
+    if (nodoHijo) {
+      enlaces.push(nodoHijo.getAttribute("href"));
     }
+  });
 
-    // Ejecutar la función cada 20 segundos
-    setInterval(actualizarEnlaceBoton, 5000); 
+  return enlaces;
+}
 
-    // Ejecutar la función al cargar la página
-    actualizarEnlaceBoton(); // Llamada inicial para actualizar al cargar
+// Obtener los enlaces y actualizar el botón
+function actualizarEnlaceBoton() {
+  const enlacesAdqva = obtenerEnlacesAdqva();
+  const boton = document.getElementById("btn");
+
+  if (enlacesAdqva.length > 0) {
+    const enlaceAleatorio =
+      enlacesAdqva[Math.floor(Math.random() * enlacesAdqva.length)];
+    boton.href = enlaceAleatorio;
+  } else {
+    console.log("No se encontraron enlaces válidos.");
+  }
+}
+
+// Ejecutar la función cada 20 segundos
+setInterval(actualizarEnlaceBoton, 5000);
+
+actualizarEnlaceBoton();
   
